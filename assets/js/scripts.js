c282fd62947a0d27604128e5f1554d4021de1a72
LisaCee <lisacanini@sandhillsdev.com>
2021-07-12 16:30:29 -0700

    #39 Not DRY but working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,31 +14,21 @@ jQuery( document ).ready( function( $ ) {
 				let lists = response.data;
 				var lists_array = Object.values( lists );
 				let text;
-				let select_lists = document.getElementById('edd_settings[eddactivecampaign_list]')
-				console.log(select_lists)
+				let select_lists;
 				if ( "checkbox" === data.format ){
 					text = '';
+					select_lists= document.getElementById( 'edd_activecampaign_checkbox_lists' );
 					for (let i = 0; i < lists_array.length; i++) {
 						text += '<input type="checkbox" name="_edd_activecampaign[]" value='+ i + '>' + lists_array[i] + '<br>';
 					}
 				} else if ( "dropdown" === data.format ) {
-					// text = ['<tr class="edd_activecampaign_lists">', '<th scope="row">', '<label for="edd_settings[eddactivecampaign_list]">Chose a list</label>', '</th>', '<td>', '<select id="edd_settings[eddactivecampaign_list]" name=edd_settings[eddactivecampaign_list]">' ];
-					text = ['<select>']
+					text = ['<select>'];
+					select_lists = document.getElementById('edd_settings[eddactivecampaign_list]');
 					for (let i = 0; i < lists_array.length; i++) {
 						text.push( '<option name="_edd_activecampaign[]" value='+ i + '>' + lists_array[i] + '</option>' );
 					}
 					text.push( '</select>' );
-					// text.push( '<p>Select the list you wish to subscribe buyers to.</p>' );
-					// text.push( '</td>' );
-
-					//add button (document.getElementById?)
-					//add p text
-					// text = text.join('');
-					console.log('THIS', text);
-					console.log(Array.isArray(text));
 				}
-				// let dd = document.getElementById("edd_settings[edd_activecampaign_list]")
-				// console.log(dd);
 				$( select_lists ).html( text );
 			}
 		})
